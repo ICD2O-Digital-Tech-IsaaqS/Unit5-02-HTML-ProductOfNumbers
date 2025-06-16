a253da2cd6f59ecf415be22283114e206d805898
Isaaq Simon <IsaaqS>
2025-06-16 14:03:01 +0000

    Addition

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Isaaq Simon All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Isaaq Simon
+// Created on: June 2025
 // This file contains the JS functions for index.html
+function multiplyByAddition() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    const output = document.getElementById("output");
+  
+    const n1 = Number(num1);
+    const n2 = Number(num2);
+  
+    // Input validation
+    if (!Number.isInteger(n1) || !Number.isInteger(n2)) {
+      output.innerHTML = "Please only enter whole numbers.";
+      return;
+    }
+  
+    if (n1 <= 0 || n2 <= 0) {
+      output.innerHTML = "Please only enter positive numbers.";
+      return;
+    }
+  
+    // Multiply using addition
+    let sum = 0;
+    for (let i = 0; i < n2; i++) {
+      sum += n1;
+    }
+  
+    output.innerHTML = `${n1} + ${n1} added ${n2} times = ${sum}`;
+  }
